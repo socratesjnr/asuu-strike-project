6d8909ba983bfc51b7440a0fb403ba85ed4dce29
Fkajose <68516668+Fkajose@users.noreply.github.com>
2023-04-25 10:54:24 +0100

    Update mailer.py
    Edited code to use secure TLS connection

--- mailing/mailer.py
+++ mailing/mailer.py
@@ -20,14 +20,28 @@ msg["To"] = TARGET_EMAIL
 msg.set_content(text_body)
 msg.add_alternative(html_body, subtype="html")
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as connection:
+
+def send_email(port):
     try:
+        if port == 587:
+            connection = smtplib.SMTP("smtp.gmail.com", port)
+            connection.ehlo()
+            connection.starttls()
+        elif port == 465:
+            connection = smtplib.SMTP_SSL("smtp.gmail.com", port)
+
         connection.login(MY_EMAIL, MY_PASSWORD)
         connection.send_message(msg)
-        print(f"Successfully sent the mail! Check!")
-    except smtplib.SMTPConnectError:
-        print(f"Hmmn. Something went wrong with the connection.")
+        connection.close()
+        print(f"Port {port}: Successfully sent the mail! Check!")
     except smtplib.SMTPAuthenticationError:
         print(
-            "Failed to send email. Your account has 2-factor Authentication enabled. You need to remove or create an application password instead. Learn more at https://support.google.com/mail/?p=InvalidSecondFactor"
+            f"Port {port}: Failed to send email. Either a wrong password/email combination or your account may have 2-Factor Authentication enabled. You need to remove or create an application password instead. Learn more at https://support.google.com/mail/?p=InvalidSecondFactor"
         )
+
+
+try:
+    send_email(587)
+
+except smtplib.SMTPConnectError:
+    send_email(465)
